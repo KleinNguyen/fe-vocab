63afb191e584bbab6b05b61cafd5674492949aa0
HuongNguyen <nguyenhuong150905@gmail.com>
2025-11-14 17:31:15 +0700

    asd

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // define backend API endpoint (URL)
 // const backendUrl = "http://localhost:3000/api/vocabs/"
-const backendUrl = "https://test.onrender.com/vocabs/"
+const backendUrl = "https://be-vocab.onrender.com/vocabs/"
 
 // declare function to call API
 export const viewAllVocabs = async()=>{
